d6f88d61135bcfba8f24b6596fcf4552701870ea
sunday-sunny <mallox22@hanyang.ac.kr>
2021-09-02 15:25:00 +0900

    빈 생명주기 @PostConstruct @PreDestory

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,17 @@
 package hello.core.lifecycle;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url){
@@ -24,8 +28,20 @@ public class NetworkClient {
     }
 
     // 서비스 종료시 호출
-    public void disconnect(){
+    public void disConnect(){
         System.out.println("close: " + url);
     }
 
+    @PostConstruct
+    public void init(){
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close(){
+        System.out.println("NetworkClient.close");
+        disConnect();
+    }
 }
